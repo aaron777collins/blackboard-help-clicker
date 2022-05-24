badbe390c1eb3bf02124496e8577e4e0408b41b6
aaron <aaron777collins@gmail.com>
2022-05-24 16:28:25 -0400

    Added the actual clicker

---
+++ BlackboardNextClicker.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+import pyautogui
+
+def run():
+	worked = False
+	confidence_amount = 1.0
+	while(worked != True):
+		try:
+			x, y = pyautogui.locateCenterOnScreen('images\\blackboard-next-clicker0.png', confidence=confidence_amount)
+			pyautogui.moveTo( (x, y), duration=0.25)
+			pyautogui.click((x, y))
+			worked = True
+		except TypeError:
+			confidence_amount-=0.1
